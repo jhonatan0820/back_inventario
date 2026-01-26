3744a45bf014bdf5fc5399e71fdd36bad900b223
jhonatan0820 <jhonizam2023@gmail.com>
2026-01-26 18:48:29 -0500

    Update database connection details in db.py

--- db.py
+++ db.py
@@ -2,9 +2,10 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="sql103.infinityfree.com",
-        user="if0_41000592",
-        password="tgda0G3JgXG",
-        database="if0_41000592_inventario"
+        host="byrui10afnftlqj38wlc-mysql.services.clever-cloud.com",
+        user="uro8vvynewtyknux",
+        password="CmNVHboKXsz0YzcrNFFh",
+        database="byrui10afnftlqj38wlc"
     )
+
 
