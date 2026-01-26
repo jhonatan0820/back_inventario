761a65ba0c8a657361a4c71fcbef734678f3ab5a
jhonatan0820 <jhonizam2023@gmail.com>
2026-01-26 18:18:53 -0500

    Change database connection settings and improve error handling
    Updated database connection details and added error handling for various routes.

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Zambrano1606",
-        database="inventario"
+        host="sql103.infinityfree.com",
+        user="if0_41000592",
+        password="if0_41000592",
+        database="if0_41000592_inventario"
     )
 
 @app.route("/GetProductos", methods=["GET"])
@@ -373,4 +373,5 @@ def delete_productos():
 
 
 if __name__ == "__main__":
+
     app.run(host="127.0.0.1", port=5000, debug=True)
