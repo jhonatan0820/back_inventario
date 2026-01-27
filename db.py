4c40943359ddd8aed0d5ec52d3d8c5ea28efb91f
jhonatan0820 <jhonizam2023@gmail.com>
2026-01-27 17:19:23 -0500

    Refactor database connection to use environment variable
    Refactored get_connection function to use DATABASE_URL from environment variables for database connection details.

--- db.py
+++ db.py
@@ -1,11 +1,23 @@
 import mysql.connector
+import os
+from urllib.parse import urlparse
 
 def get_connection():
+    database_url = os.environ.get("back-inventario")
+
+    if not database_url:
+        raise Exception("DATABASE_URL no está definida")
+
+    parsed = urlparse(database_url)
+
     return mysql.connector.connect(
-        host="byrui10afnftlqj38wlc-mysql.services.clever-cloud.com",
-        user="uro8vvynewtyknux",
-        password="CmNVHboKXsz0YzcrNFFh",
-        database="byrui10afnftlqj38wlc"
+        host=parsed.hostname,
+        port=parsed.port,
+        user=parsed.username,
+        password=parsed.password,
+        database=parsed.path.lstrip("/")
     )
+
+
 
 
