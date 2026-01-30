ce2f35c74c1919e60e0967923ce1ba6bb078e082
jhonatan0820 <jhonizam2023@gmail.com>
2026-01-29 22:16:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -386,10 +386,10 @@ def add_producto():
         cursor.execute(
             """
                 INSERT INTO productos
-                ( id_marca, id_estilo,nombre,id_estado, id_categoria)
-                VALUES (%s, %s,%s, 1, %s)
+                (nombre, id_marca, id_estilo, id_categoria, id_estado)
+                VALUES (%s, %s, %s, %s, 1)
             """,
-            (id_marca,id_estilo,nombre,id_estado,id_categoria)
+            (nombre, id_marca, id_estilo, id_categoria)
         )
         id_producto = cursor.lastrowid
 
@@ -641,6 +641,7 @@ def delete_productos():
 if __name__ == "__main__":
     import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
 
 
 
