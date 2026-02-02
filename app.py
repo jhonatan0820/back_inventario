c62e28eb5d497abb0eab552cce4645d3cb1a2c0e
jhonatan0820 <jhonizam2023@gmail.com>
2026-02-02 11:55:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -789,6 +789,10 @@ def reporte_general():
     cursor = conn.cursor(dictionary=True)
 
     try:        
+
+        def clean(val):
+        return None if val in [None, "", "null", "undefined"] else val
+        
         args = (
             clean(request.args.get('categoria')),
             celan(request.args.get('genero')),
@@ -824,6 +828,7 @@ def reporte_general():
 if __name__ == "__main__":
     import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
 
 
 
