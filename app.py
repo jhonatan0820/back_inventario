e47f0dac74e1327a41fe60b2184b28dd4e178995
Jhonatan Traslaviña <jhonizam2023@gmail.com>
2026-03-15 12:46:47 -0500

    Se agrega endpoint para generar el resumen de invenatio en pdf

--- app.py
+++ app.py
@@ -1184,8 +1184,7 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="127.0.0.1", port=port, debug=False)
 
 
